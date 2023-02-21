10fe8cd53c155537956d805430f5116c07ebfc9b
Claudia Falsetti <falsetticlaudia@gmail.com>
2023-02-21 12:20:48 +0000

    set up cors to allow for all origins

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,10 +12,11 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-
     """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
+    # resources is an object where keys are uris for a given resource. 
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
